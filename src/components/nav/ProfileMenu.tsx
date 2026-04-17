aecd3c60b3394d514e9a62641c864ee883a07549
kentchristian <kentchristiancagadas@gmail.com>
2026-04-17 15:46:52 +0800

    feat: integrate fetch user and categories - salesform

--- src/components/nav/ProfileMenu.tsx
+++ src/components/nav/ProfileMenu.tsx
@@ -1,59 +1,91 @@
-import Logout from "@mui/icons-material/Logout"
-import Settings from "@mui/icons-material/Settings"
+import Logout from '@mui/icons-material/Logout';
+import Settings from '@mui/icons-material/Settings';
 import {
   Avatar,
-  Divider,
   IconButton,
   ListItemIcon,
   Menu,
   MenuItem,
-} from "@mui/material"
-import { useState } from "react"
+} from '@mui/material';
+import { useState } from 'react';
+import { icons } from '../../lib/constants/icons';
 
 const ProfileMenu = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const open = Boolean(anchorEl)
+  const open = Boolean(anchorEl);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClose = () => {
-    setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
+  const info = ['Admin', 'Kent Christian', 'kent@gmail.com'];
+
+  type listItemsType = {
+    icons: React.ReactNode;
+    text: string;
+    fn?: () => void;
+  };
+  const listItems: listItemsType[] = [
+    {
+      icons: <icons.check size={16} />,
+      text: 'Kent Christian',
+    },
+    {
+      icons: <icons.check size={16} />,
+      text: 'kent@gmail.com',
+    },
+    {
+      icons: <icons.check size={16} />,
+      text: 'Admin',
+    },
+    {
+      icons: <Settings fontSize="small" />,
+      text: 'Settings',
+      fn: handleClose,
+    },
+    {
+      icons: <Logout fontSize="small" />,
+      text: 'Logout',
+      fn: handleClose,
+    },
+  ];
   return (
     <>
       <IconButton onClick={handleClick} size="small">
-        <Avatar sx={{ width: 32, height: 32 }}>KC</Avatar>
+        <Avatar sx={{ width: 50, height: 50 }}>KC</Avatar>
       </IconButton>
 
       <Menu
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        slotProps={{
+          paper: {
+            sx: {
+              width: 200,
+              maxWidth: '100%',
+            },
+          },
+        }}
       >
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-
-        <Divider />
-
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
+        {listItems.map((item) => (
+          <>
+            <MenuItem onClick={item.fn}>
+              <ListItemIcon>{item.icons}</ListItemIcon>
+              {item.text}
+            </MenuItem>
+          </>
+        ))}
       </Menu>
     </>
-  )
-}
+  );
+};
 
 export default ProfileMenu;
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 
 import { Typography } from './components/common/Typography';
+import ProfileMenu from './components/nav/ProfileMenu';
 import SideBar from './components/nav/SideBar';
 import { useScrollThreshold } from './lib/hooks/useScrollThreshold';
 import { useUserData } from './lib/store/useUserData';
@@ -17,13 +18,26 @@ const Layout = () => {
       <div className="flex flex-col flex-1 min-w-0 h-full">
         <header
           className={`
-          app-header flex justify-center items-center px-4 bg-(--main-bg) overflow-hidden
-          border-b border-[color:var(--sidebar-border)]
+          app-header flex justify-between items-center px-4 bg-(--main-bg) overflow-hidden
+          border-b border-(--sidebar-border)
           transition-[max-height,opacity,transform,padding] duration-300
-          ${isScrolled ? 'max-h-0 py-0 opacity-0 -translate-y-2 pointer-events-none border-transparent' : 'max-h-16 py-2 opacity-100 translate-y-0'}
+          ${isScrolled ? 'max-h-0 py-0 opacity-0 -translate-y-2 pointer-events-none border-transparent' : 'min-h-20 py-2 opacity-100 translate-y-0'}
         `}
         >
-          <Typography variant="h3">{tenant}</Typography>
+          <div style={{ width: '40px' }} aria-hidden="true" />
+
+          <Typography variant="h3" className="text-center">
+            {tenant}
+          </Typography>
+          <div
+            style={{
+              width: '40px',
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <ProfileMenu />
+          </div>
         </header>
         <main className="app-main flex-1 min-w-0 min-h-0">
           <Outlet />
